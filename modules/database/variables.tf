b5143d0ac64127e702774ada47cd3d6de9affb6c
Zufaruu <zufarbagas28@gmail.com>
2026-03-08 04:38:02 +0700

    store db credential in Secret Managers

--- modules/database/variables.tf
+++ modules/database/variables.tf
@@ -1,7 +1,2 @@
 variable "vpc_id" { type = string }
 variable "db_subnet_ids" { type = list(string) }
-variable "db_password" {
-  description = "Password untuk database RDS"
-  type        = string
-  sensitive   = true
-}
